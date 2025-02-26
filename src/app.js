f358502ddb8385ea624aa9fd6ee0d9c0e4035dbc
Vishal <vishalmaddheshiya864@gmail.com>
2025-02-26 21:08:39 +0530

    "Add Schema validation"

--- src/app.js
+++ src/app.js
@@ -1,24 +1,86 @@
 const express = require("express");
 
 const connectDB = require("./config/Database");
-
 const app = express();
 const User=require("./models/user");
-app.post("/signup",async (req,res)=>{
+const user = require("./models/user");
+app.use(express.json());
 
-    const user =new User({
-        firstName:"Virat",
-        lastName: "Kohli",
-        emailID:"viratKohli@gmail.com",
-        password:"Million@26",
-    })
+app.post("/signup",async (req,res)=>{
+    const user =new User(req.body);
+  try{
     await user.save();
-    res.send("User added successfully");
+    res.send("User Added successfully");
+  }catch(err){
+    res.status(400).send("Error saving the user:"+err.message);
+  }
 
 });
 
+app.get("/user",async (req,res)=>{
+  const UserEmail=req.body.emailID;
+  try{
+    const user = await User.find({emailID:UserEmail});
+    res.send(user);
+  }
+  catch(err)
+  {
+    res.status(400).send("something went wrong");
+  }
 
+});
 
+app.get("/feed",async (req,res)=>{
+  try{
+    const users=await User.find({});
+    res.send(users);
+  }
+  catch(err){
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  if (!userId) {
+    return res.status(400).json({ error: "User ID is required" });
+  }
+
+  try {
+    const deletedUser = await User.findByIdAndDelete(userId);
+
+    if (!deletedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully", user: deletedUser });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const { userId, ...updateData } = req.body; 
+
+  if (!userId) {
+    return res.status(400).json({ error: "User ID is required" });
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, updateData, { new: true, runValidators: true });
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({ message: "User updated successfully", user: updatedUser });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,20 +1,50 @@
-const mongoose =require("mongoose");
-const userSchema = new mongoose.Schema({
-    firstName:{
-        type:String
-    },
-    emailID:{
-        type:String
-    },
-    password:{
-        type:String
-    },
-    age:{
-        type:Number
-    },
-    gender:{
-        type:String
-    }
-});
+const mongoose = require("mongoose");
 
-module.exports=mongoose.model("User",userSchema);
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+    },
+    emailID: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: true,
+        trim: true   
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        min: 18
+    },
+    gender: {
+        type: String,
+        validate: {
+            validator: function(value) {
+                return ["male", "female", "others"].includes(value);
+            },
+            message: "Gender data is not valid"
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://www.kindpng.com/imgv/ioJmwwJ_dummy-profile-image-jpg-hd-png-download/",
+    },
+    about: {
+        type: String,
+        default: "This is the default about for the user!"
+    },
+    skill: {
+        type: [String]
+    }
+},{timestamps:true,});
+
+module.exports = mongoose.model("User", userSchema);
